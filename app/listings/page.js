21fb2daf77c849eaf75c8481678e9eca8f935f56
ribesh07 <sahribesh0123@gmail.com>
2025-06-20 18:15:48 +0545

    refactor: update layout styles for listings and main top bar

--- app/listings/page.js
+++ app/listings/page.js
@@ -199,7 +199,7 @@ const DentalSuppliesListing = () => {
 
   return (
     <>
-      <div className="max-w-7xl mx-auto p-6">
+      <div className="max-w-7xl mx-auto -my-6 p-6">
         {/* Header */}
         <div className="bg-gray-50 p-5 rounded-lg mb-5 shadow">
           <h1 className="text-2xl font-bold text-blue-900 mb-2.5">
