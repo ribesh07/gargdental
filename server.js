b5f3f0d7ec693973b4df2e4d5f903e04427a6170
ribesh07 <sahribesh0123@gmail.com>
2025-07-02 14:55:01 +0545

    feat: update API endpoints and configurations for gargdental
    - Changed all API endpoints from garg.omsok.com to gargdental.omsok.com for consistency.
    - Updated the next.config.mjs to include remote image patterns for the new domain.
    - Modified the start script in package.json to run the server in production mode.

---
+++ server.js
@@ -0,0 +1,33 @@
+import { createServer } from "http";
+import { parse } from "url";
+import next from "next";
+const dev = process.env.NODE_ENV !== "production";
+const hostname = process.env.HOSTNAME || "localhost";
+const port = process.env.PORT || 3000;
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url, true);
+      // Redirect trailing slash
+      if (req.url !== "/" && req.url.endsWith("/")) {
+        res.writeHead(301, { Location: req.url.slice(0, -1) });
+        res.end();
+        return;
+      }
+      await handle(req, res, parsedUrl);
+    } catch (err) {
+      console.error("Error occurred handling", req.url, err);
+      res.statusCode = 500;
+      res.end("Internal server error");
+    }
+  })
+    .once("error", (err) => {
+      console.error(err);
+      process.exit(1);
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`);
+    });
+});
