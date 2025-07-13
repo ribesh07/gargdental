c73450fb6273866e43e0030a3e61e917ff1bcbb8
Gyane-git <alpen850@gmail.com>
2025-07-13 17:55:25 +0545

    Refactor ForgotPasswordPage and Complains components; implement ReturnPage with multi-step form for return requests

--- app/account/forgot-password/page.js
+++ app/account/forgot-password/page.js
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
-import useInfoModalStore from "@/stores/infoModalStore";
+
 import useWarningModalStore from "@/stores/warningModalStore";
 import { baseUrl } from "@/utils/config";
 
