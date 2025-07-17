1d6e85cd4663728f1789ab1f7b2239ad1ad683cb
Gyane-git <alpen850@gmail.com>
2025-07-17 14:05:03 +0545

    Refactor AboutUs, CompanyInfo, and PrivacyPolicy components for improved data fetching and UI consistency; add Terms & Conditions page with compliance data handling

--- components/FooterBar.js
+++ components/FooterBar.js
@@ -241,7 +241,7 @@ export default function FooterBar() {
 
                 <li className="footer-list">
                   <Link
-                    href="#"
+                    href="/term&condition"
                     className="text-xs sm:text-sm hover:text-blue-200 transition-colors"
                   >
                     Terms and Conditions
