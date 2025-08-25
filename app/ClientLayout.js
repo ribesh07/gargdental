a7f1fc5a880bdeda328886cd6b5be5ef2ff22be7
ribesh07 <ribeshkumarsah@gmail.com>
2025-08-25 13:42:43 +0545

    `Refactor loading states and cache handling in InitdataFetch store and various pages`

--- app/ClientLayout.js
+++ app/ClientLayout.js
@@ -1,15 +1,15 @@
 "use client";
 import { useEffect } from "react";
-import { useProductStore , useCategoryStore } from "@/stores/InitdataFetch";
+import { useProductStore, useCategoryStore } from "@/stores/InitdataFetch";
 
 export default function ClientLayout({ children }) {
   const fetchProducts = useProductStore((s) => s.fetchProducts);
   const fetchCategories = useCategoryStore((s) => s.fetchCategories);
 
   useEffect(() => {
-    fetchProducts();     // fetch products once
-    fetchCategories();   // fetch categories once
-  }, [fetchProducts, fetchCategories]);
+    fetchProducts();
+    fetchCategories();
+  }, []); 
 
   return <div className="w-full">{children}</div>;
 }
