0fc900704059c1ad6455b01790e027a5a65385c7
Gyane-git <alpen850@gmail.com>
2025-08-25 17:34:02 +0545

    Update product grid layout to support additional columns and improve image fallback logic

--- app/hot-sales/FlashSaleProductPage.js
+++ app/hot-sales/FlashSaleProductPage.js
@@ -113,7 +113,7 @@ function FlashSaleProductPage() {
 
         {/* Products Grid */}
         <div className="max-w-7xl mx-auto px-4 mt-10">
-          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 sm-gap-x-6 gap-x-4 gap-y-4">
+          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 xl:grid-cols-6 sm-gap-x-6 gap-x-4 gap-y-4">
             {products.map((product) => (
               <ProductCardMain
                 key={product.id}
