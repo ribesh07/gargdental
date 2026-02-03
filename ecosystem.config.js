97d3f56442daccbe20a31d294154b45a417a17b5
Ribesh Kumar Sah <103181197+ribesh07@users.noreply.github.com>
2026-02-03 14:52:03 +0545

    Modify ecosystem config for server script and settings
    Updated the application configuration to use 'server.js' as the script and added additional settings for execution mode and instance count.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,18 @@ module.exports = {
   apps: [
     {
       name: "garg",
-      script: "npm",
-      args: "run start",
+      script: "server.js",
       cwd: "/home/sanjaya/development/gargdental",
+      exec_mode: "fork",
+      instances: 1,
       env: {
         NODE_ENV: "production",
-        PORT:4444
+        PORT: 4444,
       },
+      node_args: "--max-old-space-size=848",
+      watch: false,
+      autorestart: true,
+      max_restarts: 5,
     },
   ],
 };
